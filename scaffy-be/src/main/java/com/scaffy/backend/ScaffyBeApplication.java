1312ec3b020b67251655c83a1d1e6fd15ed9a7a8
filip <filipjoksovic1@gmail.com>
2026-05-04 23:42:26 +0200

    chore: set up springboot be

---
+++ scaffy-be/src/main/java/com/scaffy/backend/ScaffyBeApplication.java
@@ -0,0 +1,13 @@
+package com.scaffy.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScaffyBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScaffyBeApplication.class, args);
+	}
+
+}
---
+++ scaffy-be/src/test/java/com/scaffy/backend/ScaffyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaffy.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScaffyBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
