ff6171e8797955116f4e229be0f91ef015cfb5a9
filip <filipjoksovic1@gmail.com>
2026-06-11 21:20:11 +0200

    fix: deploy fix

--- scaffy-ops/deploy-local-api.sh
+++ scaffy-ops/deploy-local-api.sh
@@ -4,18 +4,31 @@ set -eu
 cd "$(dirname "$0")"
 
 if [ "$#" -eq 0 ]; then
-  set -- scaffy-be
+  set -- scaffy-be scaffy-analysis-worker
 fi
 
 export SCAFFY_BE_IMAGE="${SCAFFY_BE_IMAGE:-scaffy/scaffy-be:local}"
 export SCAFFY_GENERATOR_IMAGE="${SCAFFY_GENERATOR_IMAGE:-scaffy/scaffy-generator:local}"
 export SCAFFY_PUBLISHER_IMAGE="${SCAFFY_PUBLISHER_IMAGE:-scaffy/scaffy-publisher:local}"
 
+build_services=""
+for service in "$@"; do
+  case "$service" in
+    scaffy-analysis-worker)
+      build_services="${build_services} scaffy-be"
+      ;;
+    *)
+      build_services="${build_services} ${service}"
+      ;;
+  esac
+done
+
+# shellcheck disable=SC2086
 docker compose \
   --env-file .env \
   -f compose.api.yml \
   -f compose.api.build.yml \
-  build "$@"
+  build $build_services
 
 docker compose \
   --env-file .env \
